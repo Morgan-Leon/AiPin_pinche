f3e67d8aeadb82a47cb0058aab7a23c5713ca4d3
unknown <14126123@bjtu.edu.cn>
2018-09-21 17:17:32 +0800

    wxpy

--- server-wxpy/wxpy_msg.py
+++ server-wxpy/wxpy_msg.py
@@ -1,9 +1,21 @@
 from wxpy import *
+from CURD import *
+
+# 打开数据库连接
+db = pymysql.connect("localhost","lyn","lyn","aipin" )
+list = []
 
 #导入wxpy模块的全部内容
 bot=Bot(cache_path=True, console_qr=True)
 # 初始化机器人，电脑弹出二维码，用手机微信扫码登陆
 bot.groups(update=True, contact_only=False)
 #微信登陆后，更新微信群列表（包括未保存到通讯录的群）
-my_groups=bot.groups().search('16-501')
-print(my_groups)
+my_groups=bot.groups().search('锦绣')
+@bot.register(my_groups, except_self=False)
+#注册消息响应事件，一旦收到铲屎群的消息，就执行下面的代码同步消息。机器人自己在群里发布的信息也进行同步。
+def print_messages(msg):
+    print(msg)
+    if insertRoute(db,msg):
+       pass
+    else:
+        list.append(msg)
